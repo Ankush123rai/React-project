e24be3c5e49f4dfa9c6fc57bcdd380f835c602b5
Ankush123rai <ankush98rai@gamil.com>
2023-03-06 22:59:32 +0530

    react assignment

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Heading1 from './Components/Heading1';
+import Heading2 from './Components/Heading2';
+import Heading3 from './Components/Heading3';
+import Button from './Components/Button';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <section>
+        <h1>I am heading one ...from first component</h1>
+        <h2>I am heading two ...from second component</h2>
+        <h3>I am heading three ...from third component</h3>
+        <button>Click me</button>
+
+        <Heading1/>
+        <Heading2/>
+        <Heading3/>
+        <Button/>
+    </section>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
